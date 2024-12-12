608b49056e9db5e262a942120e2675d11942e76c
Elias Zwyssig <elias_zwyssig@sluz.ch>
2024-12-12 09:25:58 +0100

    formatter test

--- node-js-projekt/eslint.config.js
+++ node-js-projekt/eslint.config.js
@@ -1,14 +1,24 @@
 export default [
   {
-    files: ["**/*.js"], // Wendet die Regeln auf alle .js-Dateien an
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {
-      "no-unused-vars": "error", // Fehler bei ungenutzten Variablen
-      "prefer-const": "error",  // Fehler, wenn let statt const genutzt wird
-      "eqeqeq": "error",        // Fehler bei nicht-strikter Gleichheit
-    },
+    files:
+      [
+        '**/*.js',
+      ], // Wendet die Regeln auf alle .js-Dateien an
+    languageOptions:
+      {
+        ecmaVersion:
+          'latest',
+        sourceType:
+          'module',
+      },
+    rules:
+      {
+        'no-unused-vars':
+          'error', // Fehler bei ungenutzten Variablen
+        'prefer-const':
+          'error', // Fehler, wenn let statt const genutzt wird
+        eqeqeq:
+          'error', // Fehler bei nicht-strikter Gleichheit
+      },
   },
 ];
--- node-js-projekt/index.js
+++ node-js-projekt/index.js
@@ -1,11 +1,19 @@
 const chalk = require('chalk');
 
-console.log(chalk.red('It works'));
+console.log(
+  chalk.red(
+    'It works',
+  ),
+);
 
 const myUnusedVariable = 42; // Fehler behoben: Verwende const, da die Variable nie verändert wird.
 
-if (myUnusedVariable === 42) { // Fehler behoben: Verwende strikte Gleichheit (===)
-  console.log('This is strictly equal!');
+if (
+  myUnusedVariable ===
+  42
+) {
+  // Fehler behoben: Verwende strikte Gleichheit (===)
+  console.log(
+    'This is strictly equal!',
+  );
 }
-
-
