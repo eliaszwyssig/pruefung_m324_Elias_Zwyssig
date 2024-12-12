63fbe2fd64a73e9b705ec2127c3d9d67d58ed5e9
Elias Zwyssig <elias_zwyssig@sluz.ch>
2024-12-12 09:22:30 +0100

    linter test

--- node-js-projekt/index.js
+++ node-js-projekt/index.js
@@ -1,3 +1,10 @@
 const chalk = require('chalk');
 
 console.log(chalk.red('It works'));
+
+let myUnusedVariable = 42; // Verletzung von 'prefer-const' und 'no-unused-vars'
+
+if (myUnusedVariable == '42') { // Verletzung von 'eqeqeq'
+  console.log('This is not strictly equal!');
+}
+
